2f26d11e6adadc5298d1ff3d4f72b42e80966962
oshko <oshko.mail@gmail.com>
2025-12-09 16:50:54 -0800

    Followed the instruction and passed all test on database test

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,12 +1,27 @@
 /** @returns the employee created according to the provided details */
+import db from "#db/client";
 export async function createEmployee({ name, birthday, salary }) {
+
+  const sql = `
+  INSERT INTO employees (name, birthday, salary)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+`;
   // TODO
+  const {rows: [employee]} = await db.query(sql, [name, birthday, salary]);
+  return employee;
 }
 
 // === Part 2 ===
 
 /** @returns all employees */
 export async function getEmployees() {
+  const sql = `
+  SELECT * FROM employees
+  `;
+
+  const { rows: employees } = await db.query(sql);
+  return employees;
   // TODO
 }
 
@@ -15,6 +30,12 @@ export async function getEmployees() {
  * @returns undefined if employee with the given id does not exist
  */
 export async function getEmployee(id) {
+  const sql = `
+  SELECT * FROM employees WHERE id = ${id}
+  `;
+
+  const {rows: employee} = await db.query(sql);
+  return employee;
   // TODO
 }
 
